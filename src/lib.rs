068f0265750ca8285a076632280672e17822dc21
cchenkov <chudomir.chenkov31@gmail.com>
2022-09-16 21:13:32 +0300

    Refactoring main.rs - created new struct for camera based code logic

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod vec3;
 pub mod ray;
 pub mod sphere;
 pub mod box3;
+pub mod camera;
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use std::fs::File;
 use std::io::{BufWriter, Write};
 
 use raytracer::vec3::Vec3;
-use raytracer::ray::Ray;
 use raytracer::sphere::Sphere;
 use raytracer::box3::Box3;
+use raytracer::camera::Camera;
 
 use Vec3 as Point3;
 use Vec3 as Color;
@@ -24,21 +24,14 @@ fn main() {
     let mut file_writer = BufWriter::new(file);
 
     // camera
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = Point3::new(0.0, 0.0, 1.0);
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+    let camera = Camera::new();
 
     // ppm file format header
     write!(file_writer, "P3\n{} {}\n255\n", image_width, image_height).expect("Unable to write file");
 
     let background = Color::new(0.0, 0.0, 0.0);
-    let _sphere = Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, Color::new(1.0, 0.0, 0.0));
-    let cube = Box3::new(Point3::new(-0.25, -0.25, -0.25), Point3::new(0.25, 0.25, 0.25), Color::new(1.0, 0.0, 0.0));
+    let sphere = Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, Color::new(1.0, 0.0, 0.0));
+    let _cube = Box3::new(Point3::new(-0.25, -0.25, -0.25), Point3::new(0.25, 0.25, 0.25), Color::new(1.0, 0.0, 0.0));
 
     // render
     for i in (0..image_height).rev() {
@@ -46,9 +39,9 @@ fn main() {
             let u = f64::from(j) / f64::from(image_width - 1);
             let v = f64::from(i) / f64::from(image_height - 1);
 
-            let ray = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            let ray = camera.get_ray(u, v);
 
-            let color = match cube.hit(&ray) {
+            let color = match sphere.hit(&ray) {
                 None => background,
                 Some(color) => color
             };
